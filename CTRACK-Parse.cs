025cca19c9d5689797b4710f75b134400a43842a
Luke Arp <ruarp@microsoft.com>
2022-12-16 17:28:39 -0500

    update

--- CTRACK-Parse.cs
+++ CTRACK-Parse.cs
@@ -46,8 +46,8 @@ namespace Company.Function
                 {
                     active = "10099";
                 }
-                phone = "+1 " + entry["PHONE"].Value<string>().Substring(0,10);
-                fax = "+1 " + entry["FAX"].Value<string>().Substring(0,10);                
+                phone = "+1 " + entry["PHONE"].Value<string>().Substring(0, 10);
+                fax = "+1 " + entry["FAX"].Value<string>().Substring(0, 10);
                 if (entry["ADDR_1"].Value<string>() == "" && entry["ADDR_2"].Value<string>() != "")
                 {
                     addresses.Add(new CtrackAddress() { city = entry["CITY"].Value<string>(), line1 = entry["ADDR_2"].Value<string>(), line2 = "", zipCode = entry["ZIP"].Value<string>(), regionType = RegionIds.regions[entry["US_STATE"].Value<string>()] });
@@ -56,24 +56,31 @@ namespace Company.Function
                 {
                     addresses.Add(new CtrackAddress() { city = entry["CITY"].Value<string>(), line1 = entry["ADDR_1"].Value<string>(), line2 = entry["ADDR_2"].Value<string>(), zipCode = entry["ZIP"].Value<string>(), regionType = RegionIds.regions[entry["US_STATE"].Value<string>()] });
                 }
-                entryName = ParticipantInfo.ParseName(entry["FULL_NAME"].Value<string>());
+                if (entry["TYPEID"].Value<string>() == "10000")
+                {
+                    entryName = ParticipantInfo.ParseNameAttorney(entry["FULL_NAME"].Value<string>());
+                }
+                else
+                {
+                    entryName = ParticipantInfo.ParseNameJudge(entry["FULL_NAME"].Value<string>());
+                }
                 string barAdmit = entry["ADMIT_DATE"].Value<DateTime>().ToString("yyyy-MM-dd");
                 actors.Add(new CtrackActor() { actorTypeDetails = new CtrackActorDetails() { actorTypeID = entry["TYPEID"].Value<string>(), attorneyStatus = active, barNumber = entry["BAR_ID"].Value<string>(), barAdmittedDate = barAdmit }, firstName = entryName.firstName, middleName = entryName.middleName, lastName = entryName.lastName, addresses = addresses });
                 actorItem = new CtrackBulkRequestItem() { uri = "/v1/actors", httpMethod = "POST", requestBody = JObject.Parse((new CtrackActor() { actorTypeDetails = new CtrackActorDetails() { actorTypeID = entry["TYPEID"].Value<string>(), attorneyStatus = active, barNumber = entry["BAR_ID"].Value<string>(), barAdmittedDate = barAdmit }, firstName = entryName.firstName, middleName = entryName.middleName, lastName = entryName.lastName, addresses = addresses }).ToString()), resultName = "requestActor" + count };
                 bulkRequest.items.Add(actorItem);
                 if (phone != "+1 0000000000")
                 {
-                    bulkRequest.items.Add(new CtrackBulkRequestItem() { uri = "/v1/actors/${" + actorItem.resultName + "}/contacts", httpMethod = "POST", requestBody = JObject.Parse((new CtrackContacts() { contactTypeEntityID = "400018", contactValue = phone }).ToString()), resultName = "ContactNum" + count});
-                   // contacts.Add(new CtrackContacts() { contactTypeEntityID = "400018", contactValue = phone });
-                }                
+                    bulkRequest.items.Add(new CtrackBulkRequestItem() { uri = "/v1/actors/${" + actorItem.resultName + "}/contacts", httpMethod = "POST", requestBody = JObject.Parse((new CtrackContacts() { contactTypeEntityID = "400018", contactValue = phone }).ToString()), resultName = "ContactNum" + count });
+                    // contacts.Add(new CtrackContacts() { contactTypeEntityID = "400018", contactValue = phone });
+                }
                 if (entry["EMAIL"].Value<string>() != "")
                 {
-                    bulkRequest.items.Add(new CtrackBulkRequestItem() { uri = "/v1/actors/${" + actorItem.resultName + "}/contacts", httpMethod = "POST", requestBody = JObject.Parse((new CtrackContacts() { contactTypeEntityID = "23", contactValue = entry["EMAIL"].Value<string>() }).ToString()), resultName = "requestEmail" + count});
+                    bulkRequest.items.Add(new CtrackBulkRequestItem() { uri = "/v1/actors/${" + actorItem.resultName + "}/contacts", httpMethod = "POST", requestBody = JObject.Parse((new CtrackContacts() { contactTypeEntityID = "23", contactValue = entry["EMAIL"].Value<string>() }).ToString()), resultName = "requestEmail" + count });
                     //contacts.Add(new CtrackContacts() { contactTypeEntityID = "23", contactValue = entry["EMAIL"].Value<string>() });
                 }
-                if (fax != "+1 0000000000" && entry["TYPEID"].Value<string>() == "10000" )
+                if (fax != "+1 0000000000" && entry["TYPEID"].Value<string>() == "10000")
                 {
-                    bulkRequest.items.Add(new CtrackBulkRequestItem() { uri = "/v1/actors/${" + actorItem.resultName + "}/contacts", httpMethod = "POST", requestBody = JObject.Parse((new CtrackContacts() { contactTypeEntityID = "24", contactValue = fax }).ToString()), resultName = "requestFax" + count});
+                    bulkRequest.items.Add(new CtrackBulkRequestItem() { uri = "/v1/actors/${" + actorItem.resultName + "}/contacts", httpMethod = "POST", requestBody = JObject.Parse((new CtrackContacts() { contactTypeEntityID = "24", contactValue = fax }).ToString()), resultName = "requestFax" + count });
                     //contacts.Add(new CtrackContacts() { contactTypeEntityID = "24", contactValue = fax });
                 }
                 count++;
@@ -96,7 +103,7 @@ namespace Company.Function
 
         }
 
-        public static ParticipantInfo ParseName(string name)
+        public static ParticipantInfo ParseNameAttorney(string name)
         {
             int count = name.Split(' ').ToArray().Count();
             string given = "";
@@ -161,6 +168,71 @@ namespace Company.Function
             return new ParticipantInfo() { firstName = given, lastName = sur, middleName = middle, suffix = suffixLocal };
         }
 
+        public static ParticipantInfo ParseNameJudge(string name)
+        {
+            int count = name.Split(' ').ToArray().Count();
+            string given = "";
+            string sur = "";
+            string middle = "";
+            string suffixLocal = "";
+            string prefix = "";
+            string suffixPattern = @"^((?:[JS]r\.?|III?|IV|V|VI.*|I.*X|X))?$";
+            string prefixPattern = @"";
+            if (name != "")
+            {
+                switch (count)
+                {
+                    case 2:
+                        {
+                            sur = name.Split(' ')[1];
+                            given = name.Split(' ')[0];
+                            break;
+                        }
+                    case 3:
+                        {
+                            if (Regex.IsMatch(name.Split(' ')[2], suffixPattern))
+                            {
+                                sur = name.Split(' ')[1];
+                                given = name.Split(' ')[0];
+                                suffixLocal = name.Split(' ')[2];
+                            }
+                            else
+                            {
+                                sur = name.Split(' ')[2];
+                                given = name.Split(' ')[0];
+                                middle = name.Split(' ')[1];
+                            }
+                            break;
+                        }
+                    case 4:
+                        {
+                            if (Regex.IsMatch(name.Split(' ')[3], suffixPattern))
+                            {
+                                sur = name.Split(' ')[2];
+                                given = name.Split(' ')[0];
+                                middle = name.Split(' ')[1];
+                                suffixLocal = name.Split(' ')[3];
+                            }
+                            else
+                            {
+                                sur = name.Split(' ')[2];
+                                given = name.Split(' ')[0];
+                                middle = name.Split(' ')[1];
+                            }
+                            break;
+                        }
+                    default:
+                        {
+                            sur = name.Split(' ')[2];
+                            given = name.Split(' ')[0];
+                            middle = name.Split(' ')[1];
+                            break;
+                        }
+                }
+            }
+            return new ParticipantInfo() { firstName = given, lastName = sur, middleName = middle, suffix = suffixLocal };
+        }
+
         /*public override string ToString()
         {
             return JsonConvert.SerializeObject(this);
@@ -190,7 +262,7 @@ namespace Company.Function
 
         public CtrackBulkRequestItem()
         {
-        }        
+        }
 
         public override string ToString()
         {
